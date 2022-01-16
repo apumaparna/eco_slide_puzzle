4a05557d9c8964ffc59d886d07e6e3171d08cef0
apumaparna <aparnakumargs@gmail.com>
2022-01-16 13:48:32 -0500

    firebase added to main

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,14 @@ import 'package:eco_slide_puzzle/app/app.dart';
 import 'package:eco_slide_puzzle/bootstrap.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:eco_slide_puzzle/image preprocessing/splitImage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await splitImage('assets/images/owl.jpg', 3);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   bootstrap(() => const App());
 }
