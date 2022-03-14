9de17ce6360c9afc8ffc81ce8796a717459d9caa
apumaparna <aparnakumargs@gmail.com>
2022-03-14 13:43:05 -0400

    split gorilla image

--- lib/image preprocessing/splitImage.dart
+++ lib/image preprocessing/splitImage.dart
@@ -3,34 +3,35 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
+// import 'package:flutter/cupertino.dart';
 import 'package:image/image.dart' as img_lib;
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
 
 // void main() {
 //   WidgetsFlutterBinding.ensureInitialized();
-//   _splitImage('assets/images/solution.jpg', 4);
+//   splitImage('assets/images/gorilla.jpg', 4);
 // }
 
 Future<void> splitImage(String originalImagePath, int size) async {
   // extract the Image from the path
-  img_lib.Image? image;
+  img_lib.Image image;
 
   ByteData byteData = await rootBundle.load(originalImagePath);
   Uint8List bytes = byteData.buffer.asUint8List();
-  image = img_lib.decodeImage(bytes);
+  image = img_lib.decodeImage(bytes)!;
   // Uint8List bytes = await _readFileByte(originalImagePath);
   // image = img_lib.decodeImage(bytes);
 
   int x = 0, y = 0;
-  int width = ((image?.width)! / size).round();
-  int height = ((image?.height)! / size).round();
+  int width = ((image.width) / size).round();
+  int height = ((image.height) / size).round();
 
   // split image to parts
   List<img_lib.Image> parts = [];
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-      parts.add(img_lib.copyCrop(image!, x, y, width, height));
+      parts.add(img_lib.copyCrop(image, x, y, width, height));
       x += width;
     }
     x = 0;
@@ -48,7 +49,7 @@ Future<void> splitImage(String originalImagePath, int size) async {
   for (int i = 0; i < parts.length; i++) {
     File('$folderPath/${i.toString()}.jpg')
         .writeAsBytesSync(img_lib.encodeJpg(parts[i]));
-    // print("adding file: $folderPath/${i.toString()}.jpg");
+    print("adding file: $folderPath/${i.toString()}.jpg");
   }
 }
 
@@ -57,19 +58,19 @@ Future<String> _createDir(String folderName) async {
   final Directory _appDocDir = await getApplicationDocumentsDirectory();
   final Directory dirFolder = Directory(
       '${_appDocDir.path}/Github/eco_slide_puzzle/assets/images/$folderName');
-  // print(dirFolder.path);
+  print(dirFolder.path);
   bool directoryExists = await dirFolder.exists();
   if (directoryExists) {
     // empty the directory
-    // print("directory exists");
+    print("directory exists");
     dirFolder.deleteSync(recursive: true);
-    // print("directory deleted");
+    print("directory deleted");
   } else {
-    // print("directory does not exist");
+    print("directory does not exist");
   }
   // create the directory
   final Directory newFolder = await dirFolder.create(recursive: true);
-  // print("directory created");
+  print("directory created");
 
   return newFolder.path;
   //}
--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,27 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-
-
 import 'firebase_options.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:eco_slide_puzzle/app/app.dart';
 import 'package:eco_slide_puzzle/bootstrap.dart';
+// import 'package:eco_slide_puzzle/image preprocessing/splitImage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await splitImage('assets/images/solution.jpg', 3);
 
   try {
     await Firebase.initializeApp(
-
-        options: DefaultFirebaseOptions.currentPlatform
-
-    );
-  }catch(error){
+        options: DefaultFirebaseOptions.currentPlatform);
+  } catch (error) {
     print(error);
-    }
+  }
 
   bootstrap(() => const App());
-
 }
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   splitImage('assets/images/gorilla.jpg', 4);
+// }
