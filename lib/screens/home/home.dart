09a68752296bc5e63acf388c7db5120e7dd38727
apumaparna <aparnakumargs@gmail.com>
2022-03-13 22:29:20 -0400

    list scroll

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -17,59 +17,62 @@ class Home extends StatelessWidget {
     ButtonStyle style = ButtonStyle(
         padding: MaterialStateProperty.all(const EdgeInsets.all(50.0)));
     return Scaffold(
-      // appBar: AppBar(
-      //   automaticallyImplyLeading: true,
-      // ),
-      body: Center(
-          child: Column(children: <Widget>[
+        // appBar: AppBar(
+        //   automaticallyImplyLeading: true,
+        // ),
+        body: Center(
+      child: Column(children: <Widget>[
         const Spacer(),
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Puzzle1.routeName);
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, Puzzle1.routeName);
+                        },
+                        child: const Text('Puzzle 1'),
+                        style: style,
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, Puzzle2.routeName);
+                          },
+                          child: const Text('Puzzle 2'),
+                          style: style)),
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, LeaderBoard.routeName);
+                          },
+                          child: const Text('Leader'),
+                          style: style)),
+                ],
+              ),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () async {
+                    return context
+                        .read<AuthenticationService>()
+                        .signOut()
+                        .then((success) {
+                      if (success) {
+                        Navigator.pushNamed(context, LandingScreen.routeName);
+                      }
+                    });
                   },
-                  child: const Text('Puzzle 1'),
-                  style: style,
-                )),
-            Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, Puzzle2.routeName);
-                    },
-                    child: const Text('Puzzle 2'),
-                    style: style)),
-            Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, LeaderBoard.routeName);
-                    },
-                    child: const Text('Leader'),
-                    style: style)),
-          ],
-
-        ),
-        const Spacer(),
-        ElevatedButton(
-            onPressed: () async {
-              return context
-                  .read<AuthenticationService>()
-                  .signOut()
-                  .then((success) {
-                if (success) {
-                  Navigator.pushNamed(context, LandingScreen.routeName);
-                }
-              });
-            },
-            child: const Text('LOG OUT')),
-        const Spacer(),
-      ])),
-    );
+                  child: const Text('LOG OUT')),
+              const Spacer(),
+            ])
+      ]),
+    ));
   }
 }
